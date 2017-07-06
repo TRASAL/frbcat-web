525193593ac97ca936e8e8b75254866d0361add3
Ronald van Haren <r.vanharen@esciencecenter.nl>
2017-07-06 13:49:31 +0200

    add link in second referenceto Petroff et al.,2016 paper

--- app/components/FRBCatTXT/index.js
+++ app/components/FRBCatTXT/index.js
@@ -11,7 +11,7 @@ class FRBCatTXT extends React.Component {
           Intrinsic parameters should be taken as lower limits for FRBs with an uncertain position within a large telescope beam. Models used in this analysis are the NE2001 Galactic electron distribution (<a href="http://adsabs.harvard.edu/abs/2002astro.ph..7156C">Cordes & Lazio, 2002</a>), and the Cosmology Calculator (<a href="http://adsabs.harvard.edu/abs/2006PASP..118.1711W">Wright, 2006</a>).
           </p>
           <p>
-          If the data in this catalogue are used in a publication we ask that you cite the paper (Petroff et al., 2016) and provide the url for the FRBCAT.
+          If the data in this catalogue are used in a publication we ask that you cite the paper (<a href="http://adsabs.harvard.edu/abs/2016PASA...33...45P">Petroff et al., 2016</a>) and provide the url for the FRBCAT.
           </p>
           </div>
         );
