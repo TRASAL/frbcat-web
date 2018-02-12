7ff0d591423ce1b16548cf4b870318f1400b031f
Ronald van Haren <R.vanHaren@esciencecenter.nl>
2018-02-12 16:55:10 +0100

    update header and footer text

--- app/components/Header/index.js
+++ app/components/Header/index.js
@@ -6,7 +6,7 @@ class Header extends React.Component {
             <div className="slds-page-header" role="banner">
                 <div className="slds-grid  slds-grid--vertical-align-center">
                     <div className="slds-col">
-                        <h1 className="slds-page-header__title slds-truncate"><a href="https://github.com/AA-ALERT">
+                        <h1 className="slds-page-header__title slds-truncate"><a href="http://www.frbcat.org">
                         <img src="pics/FRBCAT-Logo-Guide-Colors-v03.png" height="112"/>
                         </a>{this.props.text}</h1>
                     </div>
